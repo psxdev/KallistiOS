26f49ee127ee0d92dd0874285878f14b6291f7f2
Paul Cercueil <paul@crapouillou.net>
2025-01-02 15:05:35 +0100

    atomics: Whitespace fixes
    Remove stray spaces that were polluting atomics.c.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- kernel/libc/c11/atomics.c
+++ kernel/libc/c11/atomics.c
@@ -4,9 +4,9 @@
    Copyright (C) 2023 Falco Girgis
 */
 
-/* This file provides the additional symbols required to provide 
-   support for C11 atomics with the "-matomic-model=soft-imask" 
-   build flag. 
+/* This file provides the additional symbols required to provide
+   support for C11 atomics with the "-matomic-model=soft-imask"
+   build flag.
 */
 
 #include <arch/arch.h>
@@ -17,8 +17,8 @@
 #include <stdbool.h>
 #include <string.h>
 
-/* Create a set of macros to codegen atomic symbols for primitive types. 
-   For these types, we simply disable interrupts then re-enable them 
+/* Create a set of macros to codegen atomic symbols for primitive types.
+   For these types, we simply disable interrupts then re-enable them
    around accesses to our atomics to ensure their atomicity.
 */
 #define ATOMIC_LOAD_N_(type, n) \
@@ -39,7 +39,7 @@
         *(type *)ptr = val; \
         irq_restore(irq); \
     }
-    
+
 #define ATOMIC_EXCHANGE_N_(type, n) \
     type \
     __atomic_exchange_##n(volatile void* ptr, type val, int model) { \
@@ -113,15 +113,15 @@ ATOMIC_FETCH_N_(unsigned long long, 8, or, |=)
 ATOMIC_FETCH_N_(unsigned long long, 8, xor, ^=)
 ATOMIC_FETCH_NAND_N_(unsigned long long, 8)
 
-/* Provide GCC with symbols and logic required to implement 
-   generically sized atomics. Rather than disabling an enabling 
-   interrupts around primitive assignments, we use spinlocks 
+/* Provide GCC with symbols and logic required to implement
+   generically sized atomics. Rather than disabling an enabling
+   interrupts around primitive assignments, we use spinlocks
    around memcpy() calls. */
 
 /* Size of each memory region covered by an individual lock. */
 #define GENERIC_LOCK_BLOCK_SIZE     (CPU_CACHE_BLOCK_SIZE * 4)
 
-/* Locks have to be shared for each page with the MMU enabled, 
+/* Locks have to be shared for each page with the MMU enabled,
    otherwise we can fail when aliasing an address range to multiple
    pages. */
 #define GENERIC_LOCK_COUNT          (PAGESIZE / GENERIC_LOCK_BLOCK_SIZE)
@@ -132,38 +132,38 @@ static spinlock_t locks[GENERIC_LOCK_COUNT] = {
 };
 
 /* Our hash function which maps an address to its corresponding lock index. */
-inline static uintptr_t 
+inline static uintptr_t
 address_to_spinlock(const volatile void *ptr) {
     return ((uintptr_t)ptr / GENERIC_LOCK_BLOCK_SIZE) % GENERIC_LOCK_COUNT;
 }
 
-void __atomic_load(size_t size, 
-                   const volatile void *ptr, 
-                   void *ret, 
+void __atomic_load(size_t size,
+                   const volatile void *ptr,
+                   void *ret,
                    int memorder) {
     const uintptr_t lock = address_to_spinlock(ptr);
 
     (void)memorder;
 
     spinlock_lock(&locks[lock]);
-    
+
     memcpy(ret, (const void *)ptr, size);
-    
+
     spinlock_unlock(&locks[lock]);
 }
 
-void __atomic_store(size_t size, 
-                    volatile void *ptr, 
-                    void *val, 
+void __atomic_store(size_t size,
+                    volatile void *ptr,
+                    void *val,
                     int memorder) {
     const uintptr_t lock = address_to_spinlock(ptr);
 
     (void)memorder;
-    
+
     spinlock_lock(&locks[lock]);
-    
+
     memcpy((void *)ptr, val, size);
-    
+
     spinlock_unlock(&locks[lock]);
 }
 
@@ -177,11 +177,11 @@ void __atomic_exchange(size_t size,
     (void)memorder;
 
     spinlock_lock(&locks[lock]);
-    
+
     memcpy(ret, (const void *)ptr, size);
     memcpy((void *)ptr, val, size);
-    
-    spinlock_unlock(&locks[lock]);                       
+
+    spinlock_unlock(&locks[lock]);
 }
 
 bool __atomic_compare_exchange(size_t size,
@@ -195,9 +195,9 @@ bool __atomic_compare_exchange(size_t size,
 
     (void)success_memorder;
     (void)fail_memorder;
-    
+
     spinlock_lock(&locks[lock]);
-    
+
     if(memcmp((const void *)ptr, expected, size) == 0) {
         memcpy((void *)ptr, desired, size);
         retval = true;
@@ -206,8 +206,8 @@ bool __atomic_compare_exchange(size_t size,
         memcpy(expected, (const void *)ptr, size);
         retval = false;
     }
-    
-    spinlock_unlock(&locks[lock]); 
+
+    spinlock_unlock(&locks[lock]);
 
     return retval;
 }
