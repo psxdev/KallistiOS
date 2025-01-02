d7ebcc825b30a0df2dd2b0bc11c503275e54509d
Paul Cercueil <paul@crapouillou.net>
2025-01-02 14:13:21 +0100

    spinlock: Transform macros into inline functions
    There is no valid reason to have the spinlock API as macros.
    Convert it to static inline functions to benefit from type checking.
    
    Note that spinlock_trylock() was moved up in the file so that it can be
    used within spinlock_lock() to factorize code.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- kernel/arch/dreamcast/include/arch/spinlock.h
+++ kernel/arch/dreamcast/include/arch/spinlock.h
@@ -30,6 +30,8 @@
 #include <sys/cdefs.h>
 __BEGIN_DECLS
 
+#include <stdbool.h>
+
 /* DC implementation uses threads most of the time */
 #include <kos/thread.h>
 
@@ -45,80 +47,74 @@ typedef volatile int spinlock_t;
 
 /** \brief  Initialize a spinlock.
 
-    This function-like macro abstracts initializing a spinlock, in case the
+    This function abstracts initializing a spinlock, in case the
     initializer is not applicable to what you are doing.
 
-    \param  A               A pointer to the spinlock to be initialized.
+    \param  lock            A pointer to the spinlock to be initialized.
 */
-#define spinlock_init(A) *(A) = SPINLOCK_INITIALIZER
+static inline void spinlock_init(spinlock_t *lock) {
+    *lock = SPINLOCK_INITIALIZER;
+}
 
 /* Note here that even if threads aren't enabled, we'll still set the
    lock so that it can be used for anti-IRQ protection (e.g., malloc) */
 
-/** \brief  Spin on a lock.
-
-    This macro will spin on the lock, and will not return until the lock has
-    been obtained for the calling thread.
-
-    \param  A               A pointer to the spinlock to be locked.
-*/
-#define spinlock_lock(A) do { \
-        spinlock_t * __lock = A; \
-        int __gotlock = 0; \
-        while(1) { \
-            __asm__ __volatile__("tas.b @%1\n\t" \
-                                 "movt %0\n\t" \
-                                 : "=r" (__gotlock) \
-                                 : "r" (__lock) \
-                                 : "t", "memory"); \
-            if(!__gotlock) \
-                thd_pass(); \
-            else break; \
-        } \
-    } while(0)
-
 /** \brief  Try to lock, without spinning.
 
-    This macro will attempt to lock the lock, but will not spin. Instead, it
+    This function will attempt to lock the lock, but will not spin. Instead, it
     will return whether the lock was obtained or not.
 
-    \param  A               A pointer to the spinlock to be locked.
-    \return                 0 if the lock is held by another thread. Non-zero if
+    \param  lock            A pointer to the spinlock to be locked.
+    \return                 False if the lock is held by another thread. True if
                             the lock was successfully obtained.
 */
-#define spinlock_trylock(A) ({ \
-        int __gotlock = 0; \
-        do { \
-            spinlock_t *__lock = A; \
-            __asm__ __volatile__("tas.b @%1\n\t" \
-                                 "movt %0\n\t" \
-                                 : "=r" (__gotlock) \
-                                 : "r" (__lock) \
-                                 : "t", "memory"); \
-        } while(0); \
-        __gotlock; \
-    })
+static inline bool spinlock_trylock(spinlock_t *lock) {
+    bool locked = false;
+
+    __asm__ __volatile__("tas.b @%1\n\t"
+                         "movt %0\n\t"
+                         : "=r"(locked)
+                         : "r"(lock)
+                         : "t", "memory");
+
+    return locked;
+}
+
+/** \brief  Spin on a lock.
+
+    This function will spin on the lock, and will not return until the lock has
+    been obtained for the calling thread.
+
+    \param  lock            A pointer to the spinlock to be locked.
+*/
+static inline void spinlock_lock(spinlock_t *lock) {
+    while(!spinlock_trylock(lock))
+        thd_pass();
+}
 
 /** \brief  Free a lock.
 
-    This macro will unlock the lock that is currently held by the calling
-    thread. Do not use this macro unless you actually hold the lock!
+    This function will unlock the lock that is currently held by the calling
+    thread. Do not use this function unless you actually hold the lock!
 
-    \param  A               A pointer to the spinlock to be unlocked.
+    \param  lock            A pointer to the spinlock to be unlocked.
 */
-#define spinlock_unlock(A) do { \
-        *(A) = 0; \
-    } while(0)
+static inline void spinlock_unlock(spinlock_t *lock) {
+    *lock = 0;
+}
 
 /** \brief  Determine if a lock is locked.
 
-    This macro will return whether or not the lock specified is actually locked
+    This function will return whether or not the lock specified is actually locked
     when it is called. This is NOT a thread-safe way of determining if a lock
     will be locked when you get around to locking it!
 
-    \param  A               A pointer to the spinlock to be checked.
+    \param  lock            A pointer to the spinlock to be checked.
+    \return                 True if the spinlock is locked, false otherwise.
 */
-#define spinlock_is_locked(A) ( *(A) != 0 )
+static inline bool spinlock_is_locked(spinlock_t *lock) {
+    return *lock != 0;
+}
 
 __END_DECLS
 
