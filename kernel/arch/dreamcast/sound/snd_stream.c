dbdbc9bf1472212954ee49dedd88f9f9129573f2
QuzarDC <quzar@completext.com>
2025-05-24 00:39:37 -0400

    Correct logical error in snd_stream double init protection.
    It was exactly backwards and never could have worked.

--- kernel/arch/dreamcast/sound/snd_stream.c
+++ kernel/arch/dreamcast/sound/snd_stream.c
@@ -323,7 +323,7 @@ int snd_stream_init(void) {
 
 int snd_stream_init_ex(int channels, size_t buffer_size) {
 
-    if(!sep_buffer[0]) {
+    if(sep_buffer[0]) {
         dbglog(DBG_ERROR, "snd_stream_init_ex(): already initialized\n");
         return -1;
     }
