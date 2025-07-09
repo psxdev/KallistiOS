8de3fb5b7faffce275bb1cbf0c33ff44c720176a
Paul Cercueil <paul@crapouillou.net>
2025-07-09 16:01:28 +0200

    dc-chain: Add 15.0.0-lra toolchain profile
    This toolchain profile can be used to test a toolchain with GCC's new
    LRA register allocator instead of the old one.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

---
+++ utils/dc-chain/profiles/dreamcast/15.0.0-lra.mk
@@ -0,0 +1,38 @@
+# Sega Dreamcast Toolchains Maker (dc-chain)
+# This file is part of KallistiOS.
+
+###############################################################################
+###############################################################################
+### THIS CONFIG IS FOR AN EXPERIMENTAL VERSION OF GCC!
+## THERE ARE NO KNOWN ISSUES BUILDING THIS VERSION as of 2025-04-17.
+###############################################################################
+###############################################################################
+
+target=sh-elf
+
+cpu_configure_args=--with-multilib-list=$(precision_modes) --with-endian=little --with-cpu=$(default_precision)
+
+# Toolchain versions for SH
+binutils_ver=2.44
+gcc_ver=15.0.0
+newlib_ver=4.5.0.20241231
+
+# Overide SH toolchain download type
+gcc_download_type=git
+gcc_git_repo=https://github.com/olegendo/gcc/
+gcc_git_branch=devel/sh-lra
+
+# GCC custom dependencies
+# Specify here if you want to use custom GMP, MPFR and MPC libraries when
+# building GCC. It is recommended that you leave this variable commented, in
+# which case these dependencies will be automatically downloaded by using the
+# '/contrib/download_prerequisites' shell script provided within the GCC packages.
+# The ISL dependency isn't mandatory; if desired, you may comment the version
+# numbers (i.e. 'isl_ver') to disable the ISL library.
+#use_custom_dependencies=1
+
+# GCC dependencies for SH
+gmp_ver=6.2.1
+mpfr_ver=4.1.0
+mpc_ver=1.2.1
+isl_ver=0.24
